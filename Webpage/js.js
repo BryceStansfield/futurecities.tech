1356e16083ca275847679009e013a12e7c7dd974
TechSupport420 <30539703+TechSupport420@users.noreply.github.com>
2017-07-29 16:30:49 +1000

    stuff

--- Webpage/js.js
+++ Webpage/js.js
@@ -1,11 +1,41 @@
 function toggleNavigation(){	
 	var navigation = document.getElementById("navigation");	
+	var drawer = document.getElementById("navigationDrawer");	
+	var map = document.getElementById("map");	
 	var visibility = navigation.style.display;
 	
-	if(visibility == "none")
+	if(visibility == "none"){
+		
 		navigation.style.display = "block";
-	else
-		navigation.style.display = "none";
+		
+		var x = 0.0;
+		var animation = setInterval( function() {
+			navigation.style.left = -400 * x;
+			drawer.style.left = 350 - x * 350;
+			map.style.left = 400 - x * 400;
+			
+			console.log(x);
+			if(x > 1){
+				clearInterval(animation);
+			}
+			x += 0.01;
+		}, 1);
+	}else{
+		var x = 0.0;
+		var animation = setInterval( function() {
+			navigation.style.left = -400 * x;
+			drawer.style.left = 350 - x * 350;
+			map.style.left = 400 - x * 400;
+			
+			console.log(x);
+			if(x > 1){
+				clearInterval(animation);
+				navigation.style.display = "none";
+			}
+			x += 0.01;
+		}, 1);
+	}
+		
 	
 }
 
