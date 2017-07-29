781e3a6b388183ff71b3f8603f3d8e11b8e6ece5
Thomas <tomcsere@gmail.com>
2017-07-29 18:27:35 +1000

    heatmap
    bryce has magic finguers

--- Webpage/map.js
+++ Webpage/map.js
@@ -11,3 +11,31 @@ function initMap() {
 		map: map
 	});
 }
+
+function massMarkerPlacement(points){
+	for(var i = 0; i < points.length; i++){
+		var curr = {lat: points[i].lat, lng: points[i].long}
+		console.log(curr);
+		var marker = new google.maps.Marker({
+			position: curr,
+			map: map
+		})
+	}
+}
+
+function massCirclePlacement(points){
+	for(var i = 0; i < points.length; i++){
+		var curr = {lat: points[i].lat, lng: points[i].long}
+		var circle = new google.maps.Circle({
+			fillColor: "#0088ff",
+			center: curr,
+			radius: points[i].radius,
+			strokeOpacity: 0.8,
+			fillOpacity: 0.2,
+			strokeColor: "white",
+			scale: 1,
+			strokeWeight: 2,
+			map: map
+		});
+	}
+}
