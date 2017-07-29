c09561e87bd37e7ba71c01290029f95ba319a1f6
TechSupport420 <30539703+TechSupport420@users.noreply.github.com>
2017-07-29 12:02:07 +1000

    More stuff

--- Webpage/js.js
+++ Webpage/js.js
@@ -1,8 +1,26 @@
-function toggleNavigation(){
-	alert("toggle navigation");
+function toggleNavigation(){	
+	var navigation = document.getElementById("navigation");	
+	var visibility = navigation.style.display;
 	
+	if(visibility == "none")
+		navigation.style.display = "block";
+	else
+		navigation.style.display = "none";
 	
 }
+
+var x = document.getElementById("demo");
+function getLocation(){
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        alert("Cannot access geo locations");
+    }
+}
+function showPosition(position) {
+	alert(position.coords.latitude +" : "+ position.coords.longitude);
+}
+
 function initMap() {
         var uluru = {lat: -25.363, lng: 131.044};
         var map = new google.maps.Map(document.getElementById('map'), {
