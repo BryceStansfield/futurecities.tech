0e78f2d3f0d8b637573adc6f756f448397c9e35a
BryceStansfield <bryce@brycestansfield.com>
2017-07-29 16:10:55 +1000

    Moving map functionality
    So we can have multiple people working at once

--- Webpage/js.js
+++ Webpage/js.js
@@ -21,18 +21,6 @@ function showPosition(position) {
 	alert(position.coords.latitude +" : "+ position.coords.longitude);
 }
 
-function initMap() {
-	var uluru = {lat: -25.363, lng: 131.044};
-	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 4,
-		center: uluru
-	});
-	var marker = new google.maps.Marker({
-		position: uluru,
-		map: map
-	});
-}
-
 function getList(file){
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
