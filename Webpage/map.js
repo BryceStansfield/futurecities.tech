f2974010cb4de75deba72865d8ab3bcb1b25f5fd
Thomas <tomcsere@gmail.com>
2017-07-30 12:02:13 +1000

    Please do not use this commit
    This commit is for colab only, DO NOT USE!

--- Webpage/map.js
+++ Webpage/map.js
@@ -104,7 +104,7 @@ function direct(from, to){
 }
 
 function cGrad(val){
-	var colourA = [0, 0, 255]; // blue
+	var colourA = [0, 255, 0]; // green
 	var colourB = [255, 0, 0]; // red
 	var colourR = [0, 0, 0];
 	for(var i = 0; i < 3; i++){
@@ -120,4 +120,38 @@ function componentToHex(c) {
 
 function rgbToHex(r, g, b) {
     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+
+function populationMap(file){
+	var results = "";
+
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			results = this.responseText;
+			results = results.split('\n');
+			for(var i = 0; i < results.length; i++){
+				list.push(results[i].split(','));
+				for(var j = 0; j < list[i].length; j++){
+					list[i][j] = Number(list[i][j]);
+				}
+			}
+
+			handler(list.slice(1,list.length-2));
+		}
+	};
+	//xhttp.open("GET", "data/" + file, true);
+	xhttp.open("GET", file, true);
+	xhttp.send();
+
+	var list = results.split("\n");
+}
+
+function intoPointArray(arr){
+	console.log(arr);
+	for(var i = 0; i < arr.length; i++){
+		arr[i] = new point(arr[i][0], arr[i][1], range);
+	}
+	points = arr;
 }
